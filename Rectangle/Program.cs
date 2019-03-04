c9c04baa137a3ddcfd91fad164c99a5101246126
Jessie Monson <jessiemonson1980@gmail.com>
2019-03-03 19:45:03 -0600

    final

--- Rectangle/Program.cs
+++ Rectangle/Program.cs
@@ -1,24 +1,25 @@
 ﻿using System;
 
-    namespace Rectangle
+namespace Rectangle
+{
 
-   
-
-
-        static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello World!");
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
             int length, width, area;
 
-        Console.Write("Enter the length of the rectangle: ");
-        length = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Enter the width of the rectangle: ");
-        width = Convert.ToInt32(Console.ReadLine());
-        area = length * width;
+            Console.Write("Enter the length of the rectangle: ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the width of the rectangle: ");
+            width = Convert.ToInt32(Console.ReadLine());
+            area = length * width;
 
-        Console.WriteLine($"Area is : {area}");
+            Console.WriteLine($"Area is : {area}");
 
-        Console.ReadKey();
+            Console.ReadKey();
+        }
     }
 }
             
